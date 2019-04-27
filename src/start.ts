f07a15d111da2e3bd03147b51304ee6ac6d11ab2
庞洪业 <panghongye@panghongye.local>
2019-04-27 10:40:57 +0800

    0

--- src/start.ts
+++ src/start.ts
@@ -1,7 +1,6 @@
 const { MongoClient, ObjectId } = require("mongodb");
-const cors = require("cors");
+// const cors = require("cors");
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const { makeExecutableSchema } = require("graphql-tools");
 const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
@@ -13,11 +12,6 @@ const MONGO_URL = "mongodb://localhost:27017/blog";
 
 const fs = require("fs");
 
-const prepare = objectId => {
-  objectId._id = objectId._id.toString();
-  return objectId;
-};
-
 const start = async () => {
   try {
     const db = await MongoClient.connect(MONGO_URL);
@@ -28,33 +22,33 @@ const start = async () => {
     const resolvers = {
       Query: {
         post: async (root, { _id }) => {
-          return prepare(await Posts.findOne(ObjectId(_id)));
+          return await Posts.findOne(ObjectId(_id));
         },
         posts: async () => {
-          return (await Posts.find({}).toArray()).map(prepare);
+          return await Posts.find({}).toArray();
         },
         comment: async (root, { _id }) => {
-          return prepare(await Comments.findOne(ObjectId(_id)));
+          return await Comments.findOne(ObjectId(_id));
         }
       },
       Post: {
         comments: async ({ _id }) => {
-          return (await Comments.find({ postId: _id }).toArray()).map(prepare);
+          return (await Comments.find({ postId: _id }).toArray()).map();
         }
       },
       Comment: {
         post: async ({ postId }) => {
-          return prepare(await Posts.findOne(ObjectId(postId)));
+          return await Posts.findOne(ObjectId(postId));
         }
       },
       Mutation: {
         createPost: async (root, args, context, info) => {
           const res = await Posts.insert(args);
-          return prepare(await Posts.findOne({ _id: res.insertedIds[1] }));
+          return await Posts.findOne({ _id: res.insertedIds[1] });
         },
         createComment: async (root, args) => {
           const res = await Comments.insert(args);
-          return prepare(await Comments.findOne({ _id: res.insertedIds[1] }));
+          return await Comments.findOne({ _id: res.insertedIds[1] });
         }
       }
     };
@@ -66,7 +60,7 @@ const start = async () => {
 
     const app = express();
 
-    app.use(cors());
+    // app.use(cors());
 
     const endpointURL = "/api/gql";
 
@@ -78,12 +72,14 @@ const start = async () => {
     app.use(endpointURL, graphqlHTTP({ schema, graphiql: false }));
 
     app.listen(5000, () => {
-      console.log(`http://localhost:${5000}${endpointURL}/ide`);
-      console.log(`http://localhost:${5000}${endpointURL}/gui`);
+      console.info(`http://localhost:${5000}${endpointURL}/ide`);
+      console.info(`http://localhost:${5000}${endpointURL}/gui`);
     });
   } catch (e) {
-    console.log(e);
+    console.error(e);
   }
 };
 
-module.exports = start;
+start();
+
+export default start;
